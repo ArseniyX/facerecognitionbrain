d970a53fe5c37a1de811a32f573fb2a17d8bf123
ArseniyX <jkl@usa.com>
2020-01-16 15:31:25 +0200

    Added Signing.js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,18 @@
 import React from "react";
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return (
         <div className={'center'}>
             <div className={'absolute mt2'}>
                 <img id={'input-image'} src={imageUrl} alt="" width={'400px'} height={'auto'}/>
-                <div className={'bounding-box'}/>
+                {document.createElement('div').style.top = box.topRow}
+                <div className={'bounding-box'} style={{
+                    top: box.topRow,
+                    bottom: box.bottomRow,
+                    right: box.rightCol,
+                    left: box.leftCol
+                }}/>
             </div>
         </div>
     );
